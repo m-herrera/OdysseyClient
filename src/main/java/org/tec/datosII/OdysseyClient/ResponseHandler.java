29329637c21e89352ff980221b3ee20d2c5e248b
Marco Herrera <m.herrera0799@gmail.com>
2018-05-02 21:35:49 -0600

    Cliente Nio

---
+++ src/main/java/org/tec/datosII/OdysseyClient/ResponseHandler.java
@@ -0,0 +1,23 @@
+package org.tec.datosII.OdysseyClient;
+
+
+public class ResponseHandler {
+    private byte[] rsp = null;
+
+    public synchronized boolean handleResponse(byte[] rsp) {
+        this.rsp = rsp;
+        this.notify();
+        return true;
+    }
+
+    public synchronized void waitForResponse() {
+        while(this.rsp == null) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+            }
+        }
+
+        System.out.println("Response:" + new String(this.rsp));
+    }
+}
