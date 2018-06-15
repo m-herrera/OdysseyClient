59031d53ee2814b3312673dad787bc51442bcff3
dario1227 <darior1227@gmail.com>
2018-06-14 22:27:47 -0600

    huffman

--- src/main/java/org/tec/datosII/OdysseyClient/Huffman/HuffmanCodeUltimate.java
+++ src/main/java/org/tec/datosII/OdysseyClient/Huffman/HuffmanCodeUltimate.java
@@ -1,8 +1,6 @@
 package org.tec.datosII.OdysseyClient.Huffman;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Node
 {
@@ -24,60 +22,58 @@ class Node
     }
 }
 
-class Huffman
-{
-    public static void encode(Node root, String str, Map<Character, String> huffmanCode)
-    {
+class Huffman {
+    public static void encode(Node root, String str, Map<Character, String> huffmanCode) {
         if (root == null)
             return;
 
         if (root.left == null && root.right == null) {
             huffmanCode.put(root.ch, str);
         }
-        encode(root.left, str+"0", huffmanCode);
+        encode(root.left, str + "0", huffmanCode);
         encode(root.right, str + "1", huffmanCode);
     }
 
-    public static void decode(Node root,String sb)
-    {
+    public static void decode(Node root, String sb) {
         String ans = "";
         Node curr = root;
-        for (int i=0;i<sb.length();i++)
-        {
+        for (int i = 0; i < sb.length(); i++) {
             if (sb.charAt(i) == '0')
                 curr = curr.left;
             else
                 curr = curr.right;
 
-            if (curr.left==null && curr.right==null)
-            {
+            if (curr.left == null && curr.right == null) {
                 ans += curr.ch;
                 curr = root;
             }
         }
-       System.out.print(ans);
+        System.out.print(ans);
     }
 
-    public static void buildHuffmanTree(String text)
-    {
+    public static void buildHuffmanTree(String text) {
         Map<Character, Integer> freq = new HashMap<>();
-        for (int i = 0 ; i < text.length(); i++) {
-            if (!freq.containsKey(text.charAt(i))) {
-                freq.put(text.charAt(i), 0);
+        char texto[] = text.toCharArray();
+        Arrays.sort(texto);
+        String data = "";
+        for (int i = 0; i < texto.length; i++) {
+            if (!freq.containsKey(texto[i])) {
+                freq.put(texto[i], 0);
+                data += texto[i];
             }
-            freq.put(text.charAt(i), freq.get(text.charAt(i)) + 1);
+            freq.put(texto[i], freq.get(texto[i]) + 1);
         }
-
-        PriorityQueue<Node> pq = new PriorityQueue<>((l, r) -> l.freq - r.freq);
-
+        LinkedList<Node> pq = new LinkedList<>();
+        char datas[] = data.toCharArray();
+        int k = 0;
         for (Map.Entry<Character, Integer> entry : freq.entrySet()) {
-            pq.add(new Node(entry.getKey(), entry.getValue()));
+            Node nodo = new Node(datas[k], entry.getValue());
+            pq.add(nodo);
+            k++;
         }
-
-        while (pq.size() != 1)
-        {
-            Node left = pq.poll();
-            Node right = pq.poll();
+        while (pq.size() != 1) {
+            Node left = ((LinkedList<Node>) pq).pollFirst();
+            Node right = ((LinkedList<Node>) pq).pollFirst();
 
             int sum = left.freq + right.freq;
             pq.add(new Node('\0', sum, left, right));
@@ -93,10 +89,9 @@ class Huffman
 
         System.out.println("\nOriginal string was :\n" + text);
 
-        String sb="";
-        for (int i = 0 ; i < text.length(); i++) {
-            sb+=huffmanCode.get(text.charAt(i));
-
+        String sb = "";
+        for (int i = 0; i < text.length(); i++) {
+            sb += huffmanCode.get(text.charAt(i));
         }
 
         System.out.println("\nEncoded string is :\n" + sb);
@@ -108,8 +103,8 @@ class Huffman
         decode(root, sb);
     }
 
-    public static void main(String[] args)
-    {
-        buildHuffmanTree("hola");
+
+    public static void main(String[] args) {
+        buildHuffmanTree("Que onda que pez");
     }
 }
